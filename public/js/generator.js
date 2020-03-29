67a36d75853e29e9fb424916086615f1d0593b0b
Nishant <singhrajnishant976@gmail.com>
2020-03-30 01:24:08 +0530

    added list-bar in slides + footer addition

--- public/js/generator.js
+++ public/js/generator.js
@@ -1,21 +1,17 @@
 var a = "1st";
 var b = "1st";
-var str ="/yeardata?year="+a+"&sem="+b;
+var str = "/yeardata?year=" + a + "&sem=" + b;
 var xhttp = new XMLHttpRequest();
 xhttp.onload = function() {
-
- if (this.readyState == 4 && this.status == 200) {
-     var d = JSON.parse(this.responseText);
-    document.getElementById("c1").innerHTML = d[0]["subjects"][0];
-    document.getElementById("c2").innerHTML = d[0]["subjects"][1];
-    document.getElementById("c3").innerHTML = d[0]["subjects"][2];
-    document.getElementById("c4").innerHTML = d[0]["subjects"][3];
-    document.getElementById("c5").innerHTML = d[0]["subjects"][4];
-    document.getElementById("c6").innerHTML = d[0]["subjects"][5];
-    
-    
- }
-  
+    if (this.readyState == 4 && this.status == 200) {
+        var d = JSON.parse(this.responseText);
+        document.getElementById("c1").innerHTML = d[0]["subjects"][0];
+        document.getElementById("c2").innerHTML = d[0]["subjects"][1];
+        document.getElementById("c3").innerHTML = d[0]["subjects"][2];
+        document.getElementById("c4").innerHTML = d[0]["subjects"][3];
+        document.getElementById("c5").innerHTML = d[0]["subjects"][4];
+        document.getElementById("c6").innerHTML = d[0]["subjects"][5];
+    }
 };
 xhttp.open("POST", str, true);
 xhttp.send();
